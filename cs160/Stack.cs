c43ceed851131e1a0678ebcb90a2888301752c60
Vincent Buchner <vincentbuchner01@gmail.com>
2023-11-10 11:54:36 -0600

    Initial commit

---
+++ cs160/Stack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace cs160;
+
+public class Stack<T>
+{   
+    ////////////////////////////////////
+    // PUBLIC AND PRIVATE ATTRIBUTES
+    ////////////////////////////////////
+    private List<T> stackList = new List<T>();
+
+    ////////////////////////////////////
+    // PUBLIC AND PRIVATE CONSTRUCTORS
+    ////////////////////////////////////
+
+    /// <summary>
+    /// Creates a new instance of a stack
+    /// </summary>
+    public Stack() {
+
+    }
+
+    /// <summary>
+    /// Creates a new instance of stack provided a list
+    /// </summary>
+    /// <param name="builtStack">The already built list</param>
+    public Stack(List<T> builtStack) {
+        stackList = builtStack;
+    }
+}
