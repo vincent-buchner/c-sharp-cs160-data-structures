43b76a2a1ecaab1dae4576220de8810d0e261a14
Vincent Buchner <vincentbuchner01@gmail.com>
2023-11-10 14:34:44 -0600

    Base implementation of a stack

--- cs160/Stack.cs
+++ cs160/Stack.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Xml.Schema;
 
 namespace cs160;
 
@@ -26,5 +27,48 @@ public class Stack<T>
     /// <param name="builtStack">The already built list</param>
     public Stack(List<T> builtStack) {
         stackList = builtStack;
+    }
+
+    /// <summary>
+    /// Pushes an item to the top of the stack
+    /// </summary>
+    /// <param name="item"></param>
+    public void Push(T item) {
+        this.stackList.Add(item);
+    }
+
+    /// <summary>
+    /// Pops an item from the top of the stack
+    /// </summary>
+    /// <returns type="T">The item popped of the stack.</returns>
+    public T Pop() {
+
+        // Gets the item to be removed, then removes it
+        T itemRemoved = this.stackList.ElementAt(this.stackList.Count - 1);
+        this.stackList.RemoveAt(this.stackList.Count - 1);
+
+        return itemRemoved;
+    }
+
+    /// <summary>
+    /// Predicate for determining whether an instance of a class is empty.
+    /// </summary>
+    /// <returns>A boolean on whether the stack is empty. If True, the stack is empty.</returns>
+    public bool IsEmpty() {
+
+        // If the list is null or have no length, then it is empty
+        if (this.stackList == null || this.stackList.Count <= 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Calculates the size of the instance of the class
+    /// </summary>
+    /// <returns>A 32-bit integer on how big the stack is.</returns>
+    public Int32 size() {
+        return this.stackList.Count;
     }
 }
